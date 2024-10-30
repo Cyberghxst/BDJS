22d5efd3334712021536b30c3cd25a87ae7c32d2
Cyberghxst <jeffreyvillatoroquezada@gmail.com>
2024-10-30 09:11:58 -0600

    add JSDoc comment to DiscordEventHandler#attach

--- src/classes/core/extended/DiscordEventHandler.ts
+++ src/classes/core/extended/DiscordEventHandler.ts
@@ -6,6 +6,11 @@ import { ClientEvents } from 'discord.js'
  * The default discord event handler.
  */
 export class DiscordEventHandler<Events extends ClientEvents = ClientEvents, Name extends keyof Events = keyof Events> extends BaseEventHandler<Events, Name> {
+    /**
+     * Attach the event to the DiscordClient.
+     * @param client - DiscordClient instance to attach the event to.
+     * @returns {void}
+     */
     public attach(client: DiscordClient) {
         client.on(this.name as keyof ClientEvents, this.call.bind(client) as any)
     }

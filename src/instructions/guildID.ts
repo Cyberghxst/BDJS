64f255da5af37814a540c2b4d86d335a62964515
Cyberghxst <jeffreyvillatoroquezada@gmail.com>
2024-11-01 12:18:32 -0600

    feat: add guild name arg to $guildID

--- src/instructions/guildID.ts
+++ src/instructions/guildID.ts
@@ -1,7 +1,9 @@
 import { BaseInstruction, ReturnType } from '@core/BaseInstruction'
+import { CallNode, LiteralNode, OperatorNode } from '@core/Nodes'
 import makeIdentifier from '@functions/makeIdentifier'
+import { type Transpiler } from '@core/Transpiler'
 import makePattern from '@functions/makePattern'
-import { LiteralNode } from '@core/Nodes'
+import { type Token } from 'akore'
 
 /**
  * @name $guildID
@@ -11,10 +13,31 @@ import { LiteralNode } from '@core/Nodes'
 export default class extends BaseInstruction {
     patterns = makePattern('$guildID')
     description = 'Returns the ID of the current guild.'
+    params = [
+        {
+            name: 'Name',
+            description: 'The name of the channel to get the ID from.',
+            type: ReturnType.String,
+            required: false,
+            spread: false
+        }
+    ]
     identifier = makeIdentifier(__filename)
     returnType = ReturnType.String
     version = '2.0.0'
-    resolve() {
-        return new LiteralNode('runtime.guild.id')
+    resolve({ inside }: Token<Transpiler>) {
+        if (!inside) return new LiteralNode('runtime.guild.id');
+
+        const [guildId] = this.splitByDelimiter(inside)
+        const serializedId = this.transpiler.resolveString(guildId).serialize()
+        
+        return new CallNode({
+            callee: new LiteralNode('runtime.client.guilds.cache.get'),
+            parameters: new OperatorNode({
+                elements: [new LiteralNode(`g => g.name === ${serializedId}`)],
+                operator: ', '
+            }),
+            zero: false
+        })
     }
 }
